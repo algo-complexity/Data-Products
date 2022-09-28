10f6043c96df11bf1628a0d66f4a9d9688a26d07
Bhargav Singapuri <bhsingapuri@gmail.com>
2022-09-28 16:31:11 +0800

    Add env config

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+import structlog
+from pydantic import BaseSettings
+
+logger = structlog.get_logger()
+
+
+class Config(BaseSettings):
+    class Config:
+        env_file_encoding = "utf-8"
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                init_settings,
+                env_settings,
+                file_secret_settings,
+            )
+
+    debug: bool = True
+
+    database_name: str
+    database_user: str
+    database_password: str
+    database_host: str
+    database_port: int = 5432
+
+
+# Lazily initialize the config variable using module-level __getattr__
+# so that we can import the Config class without triggering config load.
+_config = None
+
+
+def __getattr__(name):
+    if name == "config":
+        global _config
+        if _config is None:
+            _config = load_config()
+        return _config
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def load_config():
+    env_file = os.getenv("ENV_FILE", ".env")
+    return Config(_env_file=env_file)
